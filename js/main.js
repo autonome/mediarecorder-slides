ab3de2ae4be98850cd8f387f7926fae45faaf268
dietrich ayala <autonome@gmail.com>
2017-02-01 21:48:53 +0630

    add server, client socket

--- js/main.js
+++ js/main.js
@@ -1,4 +1,67 @@
 window.onload = function() {
-	var sections = Array.from(document.querySelectorAll('section'));
-	var deck = new MVSD(sections);
+  var sections = Array.from(document.querySelectorAll('section'));
+  var deck = new MVSD(sections);
 };
+
+var firstRun = localStorage.firstRun || true;
+
+function onDCL() {
+
+  // initialize websocket connection
+  var server = 'localhost', //window.location.hostname,
+      port = 8009,
+      socketURL = 'ws://' + server + ':' + port,
+      socket = new WebSocket(socketURL);
+
+  socket.onopen = function() {
+
+    // ensures binary sends work correctly
+    socket.binaryType = "arraybuffer";
+
+    // register with server
+    //socket.send(JSON.stringify({ 'firstRun': firstRun }));
+
+    socket.onmessage = function(msg) {
+      console.log('ws message!!!')
+      var obj = JSON.parse(msg.data);
+      console.log('ws message:', obj)
+    };
+  };
+
+  /*
+  // initialize camera source
+  var vid = document.querySelector('#vid');
+  cameraSource.start({
+    videoElement: vid,
+    callback: function() {
+      console.log('videoooo')
+    }
+  });
+
+  var button = document.createElement('button')
+  button.innerText = 'take snapshot'
+  button.addEventListener('click', function() {
+    // uncomment for trashbeaty v1 testing
+    //cameraSource.snapshot();
+
+    cameraSource.snapshotToCanvas(function(canvas) {
+      // local test
+      var img = document.createElement('img')
+      img.src = canvas.toDataURL()
+      document.body.appendChild(img)
+
+      var context = canvas.getContext('2d'),
+          image = context.getImageData(0, 0, canvas.width, canvas.height),
+          buffer = new ArrayBuffer(image.data.length),
+          bytes = new Uint8Array(buffer);
+      for (var i = 0; i < bytes.length; i++) {
+          bytes[i] = image.data[i];
+      }
+      socket.send(buffer);
+    });
+  });
+  document.body.appendChild(button)
+  */
+
+}
+window.addEventListener('DOMContentLoaded', onDCL);
---
+++ server/index.js
@@ -0,0 +1,61 @@
+/* globals require */
+"use strict";
+
+const httpPort = 8000;
+const ws = require("ws");
+const wsPort = 8009;
+const express = require("express");
+
+(() => {
+
+  // Web server
+  var app = new express();
+
+  //app.disable('etag');
+
+  // Register static dir
+  app.use('/', express.static('../www'));
+
+  app.listen(httpPort, '0.0.0.0', function() {
+    console.log('Web server listening on port ' + httpPort);
+  });
+  
+  // Web socket server
+  var wss = new ws.Server({ port: wsPort });
+  wss.on('connection', function(conn) {
+
+    // Upon each new connection, message all clients
+    app.use(function(req, res, next) {
+      console.log('ws headers', req.headers);
+
+      conn.send({
+        'headers': req.headers
+      });
+    });
+
+    console.log('sent headers')
+
+    conn.on('message', function(msg) {
+
+      console.log('message received');
+
+      if (msg.blink) {
+
+        // Make stuff blink.
+
+      }
+
+      conn.send('from server');
+
+    });
+
+  });
+
+  const shutdown = () => {
+    console.log("shutting down...");
+    process.exit(0);
+  };
+
+  process.on("SIGTERM", shutdown);
+  process.on("SIGINT", shutdown);
+})();
