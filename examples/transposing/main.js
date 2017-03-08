8542be8f64b5bf9c14864809f0062dfdbc33c01e
dietrich ayala <autonome@gmail.com>
2017-03-08 22:12:26 +0545

    digital nepal day 1

--- examples/transposing/main.js
+++ examples/transposing/main.js
@@ -32,7 +32,7 @@ window.onload = function () {
 			recordSomething();
 		}, 100);
 		
-		setInterval(changeToRandomPitch, randomPitchInterval);
+		//setInterval(changeToRandomPitch, randomPitchInterval);
 
 	});
 };
